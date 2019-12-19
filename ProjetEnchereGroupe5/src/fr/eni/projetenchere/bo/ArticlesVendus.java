37b329ba2d609439ee2710cf0e0f1075956d1126
YatiFheng <59048527+YatiFheng@users.noreply.github.com>
2019-12-19 11:14:28 +0100

    Bo Encheres

---
+++ ProjetEnchereGroupe5/src/fr/eni/projetenchere/bo/ArticlesVendus.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package fr.eni.projetenchere.bo;
+
+/**
+ * Classe en charge de 
+ * @author kreine2019
+ * @version ProjetEnchereGroupe5 - v1.0
+ * @date 19 déc. 2019 - 11:11:15
+ */
+public class ArticlesVendus {
+
+	
+	
+	
+}
---
+++ ProjetEnchereGroupe5/src/fr/eni/projetenchere/bo/Encheres.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package fr.eni.projetenchere.bo;
+
+import java.util.Date;
+
+public class Encheres {
+
+	private Date dateEnchere;
+	private int montantEnchere;
+	private int noArticle;
+	private int noUtilisateur;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
